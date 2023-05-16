f8ebcfdd236319f3e5bd21bccd8009bf8b82c45e
Julian <julian_sunpacyo@dlsu.edu.ph>
2023-05-15 22:44:28 -0700

    Click outside to close drop down

--- src/components/FilterItem/filteritem.tsx
+++ src/components/FilterItem/filteritem.tsx
@@ -1,5 +1,6 @@
 import styles from './filteritem.module.scss';
 import { BiChevronDown } from "react-icons/bi";
+import ClickAwayListener from '@mui/material/ClickAwayListener';
 
 interface Props {
     title: string;
@@ -10,25 +11,26 @@ interface Props {
 }
 
 const FilterItem = (props:Props) => {
-
     return (
-        <div>
-            <p className={styles.filterName} onClick={() => props.clickFunction(props.toggleSetter, props.isClicked)}>{props.title}{<BiChevronDown className={styles.filterIcon} style ={{transform:props.isClicked ? 'rotate(180deg)' : 'rotate(0deg)', transition:'transform 1s ease'}}/>}</p>
-            <div className={styles.optionsContainer} style={{visibility: (props.isClicked?'visible':'hidden')}}>
-                <p className={styles.optionName}>
-                    All
-                </p>
-                <p className={styles.optionName}>
-                    Tea
-                </p>
-                <p className={styles.optionName}>
-                    Coffee
-                </p>
-                <p className={styles.optionName}>
-                    Sacramento, CA
-                </p>
+        <ClickAwayListener onClickAway={() => props.toggleSetter(false)}>
+            <div>
+                <p className={styles.filterName} onClick={() => props.clickFunction(props.toggleSetter, props.isClicked)}>{props.title}{<BiChevronDown className={styles.filterIcon} style ={{transform:props.isClicked ? 'rotate(180deg)' : 'rotate(0deg)', transition:'transform 1s ease'}}/>}</p>
+                <div className={styles.optionsContainer} style={{visibility: (props.isClicked?'visible':'hidden')}}>
+                    <p className={styles.optionName}>
+                        All
+                    </p>
+                    <p className={styles.optionName}>
+                        Tea
+                    </p>
+                    <p className={styles.optionName}>
+                        Coffee
+                    </p>
+                    <p className={styles.optionName}>
+                        Sacramento, CA
+                    </p>
+                </div>
             </div>
-        </div>
+        </ClickAwayListener>
     )
 }
 
