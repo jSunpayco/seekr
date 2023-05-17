535bf0f34586ecbb882badd66598c232889554c1
Julian <julian_sunpacyo@dlsu.edu.ph>
2023-05-16 22:06:54 -0700

    Initial jobs grid

--- src/pages/Jobs/jobs.tsx
+++ src/pages/Jobs/jobs.tsx
@@ -27,16 +27,58 @@ const Jobs = () => {
             Title:"Entry Level Automation Test Engineer",
             Type:"Full Time",
             URL:"https://www.google.com/"
+        },
+        {
+            jobid:1,
+            date:"03/14/2023",
+            Category:"SDET",
+            Company:"Hip Sofwares",
+            Location:"Boston, MA",
+            Status:"Interviewing",
+            Title:"Entry Level Automation Test Engineer",
+            Type:"Full Time",
+            URL:"https://www.google.com/"
+        },
+        {
+            jobid:1,
+            date:"03/14/2023",
+            Category:"SDET",
+            Company:"Hip Sofwares",
+            Location:"Boston, MA",
+            Status:"Interviewing",
+            Title:"Entry Level Automation Test Engineer",
+            Type:"Full Time",
+            URL:"https://www.google.com/"
+        },
+        {
+            jobid:1,
+            date:"03/14/2023",
+            Category:"SDET",
+            Company:"Hip Sofwares",
+            Location:"Boston, MA",
+            Status:"Interviewing",
+            Title:"Entry Level Automation Test Engineer",
+            Type:"Full Time",
+            URL:"https://www.google.com/"
         }
     ]
 
     const [myJobs, setMyJobs] = useState(jobsSample)
+
+    const jobsContainer = () => {
+        return myJobs.map((item, index) => (
+            <div className={styles.jobContainer}></div>
+        ))
+    }
 
     return (
         <div className={styles.jobsContainer}>
             <Navigation></Navigation>
             <h1 className={styles.pageTitle}>My Applications</h1>
             <h1 className={styles.warning}>This page is currently under progress</h1>
+            <div className={styles.jobsGrid}>
+                {jobsContainer()}
+            </div>
         </div>
     )
 }
