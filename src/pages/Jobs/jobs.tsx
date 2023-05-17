25c68ea3b92eb28bfb872d7315b0d1f3b8b70224
Julian <julian_sunpacyo@dlsu.edu.ph>
2023-05-16 21:42:06 -0700

    Jobs page title + jobs dummy data

--- src/pages/Jobs/jobs.tsx
+++ src/pages/Jobs/jobs.tsx
@@ -1,11 +1,41 @@
 import styles from './jobs.module.scss';
 import Navigation from "../../components/Navigation/navigation";
 
+import { useState } from 'react';
+
 const Jobs = () => {
+
+    const jobsSample = [
+        {
+            jobid:0,
+            date:"04/25/2023",
+            Category:"SWE",
+            Company:"Techie inc",
+            Location:"Davis, CA",
+            Status:"Sent",
+            Title:"Junior Software Engineer",
+            Type:"Full Time",
+            URL:"https://www.google.com/"
+        },
+        {
+            jobid:1,
+            date:"03/14/2023",
+            Category:"SDET",
+            Company:"Hip Sofwares",
+            Location:"Boston, MA",
+            Status:"Interviewing",
+            Title:"Entry Level Automation Test Engineer",
+            Type:"Full Time",
+            URL:"https://www.google.com/"
+        }
+    ]
+
+    const [myJobs, setMyJobs] = useState(jobsSample)
 
     return (
         <div className={styles.jobsContainer}>
             <Navigation></Navigation>
+            <h1 className={styles.pageTitle}>My Applications</h1>
             <h1 className={styles.warning}>This page is currently under progress</h1>
         </div>
     )
