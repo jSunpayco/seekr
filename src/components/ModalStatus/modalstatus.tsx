f74e1dbe3b016606fa11b7d710a34b9fd92edbc3
Julian <julian_sunpacyo@dlsu.edu.ph>
2023-05-20 21:59:42 -0700

    Options drop down
    styling and initial logic

--- src/components/ModalStatus/modalstatus.tsx
+++ src/components/ModalStatus/modalstatus.tsx
@@ -13,11 +13,36 @@ const ModalStatus = (props:Props) => {
 
     const [check, setCheck] = useState(false)
     const [check1, setCheck1] = useState(false)
+    
+    const [drop1, setDrop1] = useState(false)
+
+    const unCheck = (radioButton:Dispatch<SetStateAction<boolean>>, radioState:boolean, dropdownState:boolean, dropDown:Dispatch<SetStateAction<boolean>>) => {
+        radioButton(!radioState)
+        if(dropdownState){
+            dropDown(false)
+        }
+    }
 
     const handleDataListClick = (event:React.MouseEvent<HTMLDivElement>, state:boolean) => {
-        if(state)
+        if(state){
+            setDrop1(!drop1)
             event.stopPropagation();
-      };
+        }
+    };
+
+    const handleDataListOptionClick = (event:React.MouseEvent<HTMLDivElement>, newString:string) => {
+            setCurrProgress(newString)
+            event.stopPropagation();
+    };
+    
+    const progressList = ['Sent', 'Assessment', 'Interviewing']
+    const [currProgress, setCurrProgress] = useState('Sent')
+
+    const datalistOption = (options:string[]) => {
+        return options.map((item, index)=>(
+            <p id={`statusOption ${index}`} className={styles.datalistItem} onClick={(e)=>handleDataListOptionClick(e, item)}>{item}</p>
+        ))
+    }
 
     return (
         <div className={styles.modalGreyScreen}>
@@ -27,14 +52,17 @@ const ModalStatus = (props:Props) => {
                     <button className={styles.modalClose} onClick={()=>props.closeFunction(false)}>X</button>
                 </div>
 
-                <div className={styles.optionContainer} onClick={()=>setCheck1(!check1)} style={{backgroundColor:(check1?'#e6e6e6':'')}}>
+                <div className={styles.optionContainer} onClick={()=>unCheck(setCheck1, check1, drop1, setDrop1)} style={{backgroundColor:(check1?'#e6e6e6':'')}}>
                     <label className={styles.label}>
                         <input type="radio" name="radio" checked={check1}/>
                         <span className={styles.check}></span>
                     </label>
                     <div className={styles.datalistContainer} onClick={(e) => handleDataListClick(e, check1)} style={{color:(check1?'black':'#a4a3a4'), border:(check1?'solid 1px black':'solid 1px #a4a3a4')}}>
-                        Something
+                        {currProgress}
                         <BiChevronDown className={styles.menuArrow}/>
+                    </div>
+                    <div className={styles.datalistOptions} style={{visibility:(drop1?'visible':'hidden')}}>
+                        {datalistOption(progressList)}
                     </div>
                 </div>
             </form>
