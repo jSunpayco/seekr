46d4d30cd7dcaa563a7419b53b027999ba308e57
Julian <julian_sunpacyo@dlsu.edu.ph>
2023-06-13 23:12:08 -0700

    Add register page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Login from './pages/Login/login'
 import Jobs from './pages/Jobs/jobs';
+import Register from './pages/Register/register';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/jobs" element={<Jobs />} />
       </Routes> 
     </Router>
