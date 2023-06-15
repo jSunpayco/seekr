f9978c8fcd481a73f4621dbf828108b2e5139ee8
Julian <julian_sunpacyo@dlsu.edu.ph>
2023-06-15 01:21:12 -0700

    company/title search logic

--- src/components/SearchBar/searchbar.tsx
+++ src/components/SearchBar/searchbar.tsx
@@ -1,15 +1,21 @@
 import styles from './searchbar.module.scss';
 
 interface Props {
-    jobId?:number;
+    valueFunction:React.Dispatch<React.SetStateAction<string>>;
 }
 
 const SearchBar = (props:Props) => {
 
+    const onHandleValueChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setTimeout(() => {
+            props.valueFunction(e.target.value)
+        }, 300);
+    }
+
     return (
         <div className={styles.searchBarContainer}>
             <h2 className={styles.searchBarLabel}>Search</h2>
-            <input className={styles.searchBar} placeholder='Title/Company'></input>
+            <input className={styles.searchBar} placeholder='Title/Company' onChange={(e)=>onHandleValueChange(e)}></input>
         </div>
     )
 }
