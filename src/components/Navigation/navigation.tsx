9e53c40a2c3a954e1d17e6bea86ac36e8f50fe92
Julian <julian_sunpacyo@dlsu.edu.ph>
2023-06-15 17:25:29 -0700

    Pair mobile and desktop checkboxes

--- src/components/Navigation/navigation.tsx
+++ src/components/Navigation/navigation.tsx
@@ -109,11 +109,19 @@ const Navigation = (props:Props) => {
         }
     ]
 
+    const handleCheckboxClick = (checked:boolean, item:string, name:string, boxId:string) => {
+        const checkbox1 = document.getElementById(boxId) as HTMLInputElement;
+        if(checkbox1!=null)
+            checkbox1.checked = !checkbox1.checked;
+
+        props.boxClick(item, name, checked)
+    }
+
     const filterOptions = (options:string[], name:string) => {
         return options.map((item, index) => (
             <div key={`option${item}${index}`} id={`option${item}${index}`} className={styles.mobileOption}>
                 <label htmlFor={`mobileOptionInput${item}${index}`} className={styles.mobileOptionLabel}>
-                    <input id={`mobileOptionInput${item}${index}`} type="checkbox" value={item} className={styles.mobileOptionInput} onChange={(e)=>props.boxClick(item, name, e.target.checked)}/>
+                    <input id={`mobileOptionInput${item}${index}`} type="checkbox" value={item} className={styles.mobileOptionInput} onChange={(e)=>handleCheckboxClick(e.target.checked, item, name, `optionInput${item}${index}`)}/>
                     {item}
                 </label>
             </div>
@@ -123,7 +131,7 @@ const Navigation = (props:Props) => {
     const desktopFilterOptions = (options:string[], name:string) => {
         return options.map((item, index) => (
             <label key={`optionInput${item}${index}`} htmlFor={`optionInput${item}${index}`} className={styles.desktopOptionLabel} style={{marginBottom:(index===options.length-1?'5%':'0')}}>
-                <input id={`optionInput${item}${index}`} type="checkbox" value={item} className={styles.desktopOptionInput} onChange={(e)=>props.boxClick(item, name, e.target.checked)}/>
+                <input id={`optionInput${item}${index}`} type="checkbox" value={item} className={styles.desktopOptionInput} onChange={(e)=>handleCheckboxClick(e.target.checked, item, name, `mobileOptionInput${item}${index}`)}/>
                 {item}
             </label>
         ))
