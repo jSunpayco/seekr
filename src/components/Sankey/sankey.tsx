834d7575ab3adbd98e6e26e5b8055db134c42164
Julian <julian_sunpacyo@dlsu.edu.ph>
2023-09-03 11:02:51 -0700

    Display a sankey diagram

--- src/components/Sankey/sankey.tsx
+++ src/components/Sankey/sankey.tsx
@@ -1,12 +1,120 @@
+import * as d3Sankey from 'd3-sankey';
+import * as d3 from 'd3'
 import styles from './sankey.module.scss';
+import { useEffect, useState } from 'react';
 
 interface Props {
     isOpen: boolean;
     closeFunction: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+interface SNodeExtra {
+    nodeId: number;
+    name: string;
+}
+
+interface SLinkExtra {
+    source: number;
+    target: number;
+    value: number;
+    uom: string;
+}
+type SNode = d3Sankey.SankeyNode<SNodeExtra, SLinkExtra>;
+type SLink = d3Sankey.SankeyLink<SNodeExtra, SLinkExtra>;
+
+interface SankeyDataType {
+    nodes: SNode[];
+    links: SLink[];
+}
+
 const ModalSankey = (props:Props) => {
 
+    const sankeyData: SankeyDataType = {
+        nodes:[{
+            nodeId: 0,
+            name: "In Progress"
+        }, {
+            nodeId: 1,
+            name: "Rejected"
+        }],
+        links: [{
+            source: 0,
+            target: 1,
+            value: 2,
+            uom: 'Widget(s)'
+        }]
+    }
+
+
+    useEffect(() => {
+        DrawChart();
+    }, []);
+
+    function DrawChart() {
+
+        var svg = d3.select("#sankey"),
+            width = 320,
+            height = 280;
+
+        var formatNumber = d3.format(",.0f"),
+            format = function (d: any) { return formatNumber(d) + " TWh"; },
+            color = d3.scaleOrdinal(d3.schemeCategory10);
+
+        var sankey = d3Sankey.sankey()
+            .nodeWidth(15)
+            .nodePadding(10)
+            .extent([[1, 1], [width - 1, height - 6]]);
+
+        var link = svg.append("g")
+            .attr("class", "links")
+            .attr("fill", "none")
+            .attr("stroke", "#000")
+            .attr("stroke-opacity", 0.2)
+            .selectAll("path");
+
+        var node = svg.append("g")
+            .attr("class", "nodes")
+            .attr("font-family", "sans-serif")
+            .attr("font-size", 10)
+            .selectAll("g");
+
+
+        sankey(sankeyData);
+
+        link = link
+            .data(sankeyData.links)
+            .enter().append("path")
+            .attr("d", d3Sankey.sankeyLinkHorizontal())
+            .attr("stroke-width", function (d: any) { return Math.max(1, d.width); });
+
+        link.append("title")
+            .text(function (d: any) { return d.source.name + " → " + d.target.name + "\n" + format(d.value); });
+
+        node = node
+            .data(sankeyData.nodes)
+            .enter().append("g");
+
+        node.append("rect")
+            .attr("x", function (d: any) { return d.x0; })
+            .attr("y", function (d: any) { return d.y0; })
+            .attr("height", function (d: any) { return d.y1 - d.y0; })
+            .attr("width", function (d: any) { return d.x1 - d.x0; })
+            .attr("fill", function (d: any) { return color(d.name.replace(/ .*/, "")); })
+            .attr("stroke", "#000");
+
+        node.append("text")
+            .attr("x", function (d: any) { return d.x0 - 6; })
+            .attr("y", function (d: any) { return (d.y1 + d.y0) / 2; })
+            .attr("dy", "0.35em")
+            .attr("text-anchor", "end")
+            .text(function (d: any) { return d.name; })
+            .filter(function (d: any) { return d.x0 < width / 2; })
+            .attr("x", function (d: any) { return d.x1 + 6; })
+            .attr("text-anchor", "start");
+
+        node.append("title")
+            .text(function (d: any) { return d.name + "\n" + format(d.value); });
+    }
 
     const greyAreaClickFunction = (event:React.MouseEvent<HTMLDivElement>) => {
         if (event.target === event.currentTarget)
@@ -20,6 +128,7 @@ const ModalSankey = (props:Props) => {
                     <h1 className={styles.modalTitle}>Sankey Diagram</h1>
                     <button className={styles.modalClose} onClick={()=>props.closeFunction(false)}>X</button>
                 </div>
+                <div id="sankeyContainer"><svg id="sankey" width="960" height="500"></svg></div>
             </div>
         </div>
     )
