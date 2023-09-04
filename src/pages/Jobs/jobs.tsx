336256a73f80954fda0054a9b00fbb2830b4ad63
Julian <julian_sunpacyo@dlsu.edu.ph>
2023-09-04 11:38:41 -0700

    Fix legend color

--- src/pages/Jobs/jobs.tsx
+++ src/pages/Jobs/jobs.tsx
@@ -17,7 +17,7 @@ import { Statuses } from '../../interfaces/Statuses';
 
 const Jobs = () => {
     
-    const statusLegend = ["In Progress", "Rejected", "Offer"];
+    const statusLegend = ["In Progress", "Reject", "Offer"];
 
     const legendItems = () => {
         return statusLegend.map((item, index) => (
